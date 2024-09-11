b562017f81ca1e8ec8aee9b4cc2cc4b3b833b077
Polinakhot <polinak9@nycstudents.net>
2024-09-10 23:03:25 -0400

    Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 7;
+        double b = 2;
+        boolean c = True;
+      
+        String d = "toucan";
+      
+        System.out.println(a + b);
+
+        int a = 9;
+
+        int e = 20;
+
+        System.out.println(e);
+
+      
+    }
+}
